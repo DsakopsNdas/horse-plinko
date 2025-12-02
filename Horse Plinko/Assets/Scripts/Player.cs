bfe5490e9f4e86ef6c68c6fb0c0485357713bfa0
Daniel <daniel.nguyen8@mohawkcollege.ca>
2025-12-02 17:49:35 -0500

    Set TargetFPS, create disc prefab, and edit player class

---
+++ Horse Plinko/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject horse;
+    public float speed = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        Vector3 offset = new Vector3(movementX, 0, 0);
+        transform.position += offset;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(horse, transform.position, transform.rotation);
+        }
+    }
+}
